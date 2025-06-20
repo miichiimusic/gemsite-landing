54357cdb5785c04f8863fd75bcc52f98059737b7
YourGitHubUsername <your-email@example.com>
2025-06-20 09:26:23 -0400

    h1 black

--- gemsite-landing.tsx
+++ gemsite-landing.tsx
@@ -21,7 +21,7 @@ export default function Component() {
           <h1
   className="
     text-5xl md:text-6xl lg:text-7xl
-    font-extrabold tracking-tight
+    font-bold tracking-tight
     leading-tight md:leading-snug
     text-left
   "
